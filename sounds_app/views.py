a66c627265c9a43253c326b0451b4d20104611ca
RytisDr <r.drazdauskas@gmail.com>
2020-05-25 22:19:32 +0200

    Moves API responses into api.py.

--- sounds_app/views.py
+++ sounds_app/views.py
@@ -3,8 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from .serializers import RegistrationSerializer
-from rest_framework.permissions import AllowAny
-# Create your views here.
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 
 @api_view(['POST'])
